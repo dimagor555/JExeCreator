0362e4ee1c8622729f8f5ea3b271694e58b21add
Dmitryi Goryachev <dimagor555@mail.ru>
2020-03-20 14:53:26 +0300

    delete console logging

--- src/main/java/ru/dimagor555/jexecreator/CommandsHandler.java
+++ src/main/java/ru/dimagor555/jexecreator/CommandsHandler.java
@@ -19,7 +19,6 @@ public class CommandsHandler {
 
     private void executeCommand(String[] args) {
         Commands command = getCommand(args[0]);
-        System.out.println(command.name());
         switch(command) {
             case HELP:
                 Main.console.printHelp();
